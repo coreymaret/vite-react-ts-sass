4f0947a9a222d75ed14f0718586c7989344244c9
coreymaret <coreymaret@gmail.com>
2024-06-28 09:02:41 -0400

    App.tsx edited with proper function to display content. Still need to check for proficiency

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './src/components/NavBar';
 import Footer from './src/components/Footer';
 import Home from './src/pages/Home';
@@ -7,7 +7,7 @@ import About from './src/pages/About';
 import Services from './src/pages/Services';
 import Contact from './src/pages/Contact';
 import NotFound from './src/pages/NotFound';
-
+/*
 const App: React.FC = () => {
   return (
     <Router>
@@ -23,5 +23,21 @@ const App: React.FC = () => {
     </Router>
   );
 };
+*/
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+    <NavBar />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route Component={NotFound} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  )}
 
 export default App;
